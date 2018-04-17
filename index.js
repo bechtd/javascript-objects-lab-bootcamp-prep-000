e1b28e62e8f297e17cb14892bd0055c119bb8817
David Becht <bechtd@gmail.com>
2018-04-17 18:27:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,14 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value;
   return object;
 }
+
+function deleteFromObjectByKey(object, key) {
+  var newObject = Object.assign({}, object);
+  delete newObject[key];
+  return newObject;
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  object[key] = value;
+  return object;
+}
